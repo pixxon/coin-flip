361a15b734ec3c9def082ac7b14050a0c600ba31
pixxon <szitar.gergo@gmail.com>
2022-08-19 10:49:09 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 import './style.css';
 
 let currentDegrees = 0;
-let amountRedAmount = 0;
-let amountBlueAmount = 0;
 let isFlipping = false;
 
 coin.onclick = () => {
-    if (isFlipping) return;
+  if (isFlipping) return;
 
-    coin.classList.add("flipping");
-    isFlipping = true;
-    const random = Math.floor(Math.random() * 4 + 9); 
+  coin.classList.add('flipping');
+  isFlipping = true;
+  const random = Math.floor(Math.random() * 4 + 9);
 
-    currentDegrees += 180 * random;
+  currentDegrees += 180 * random;
 
-    if (currentDegrees % 360 === 0) {
-        amountBlueAmount++;
-    } else {
-        amountRedAmount++;
-    }
+  coin.style.transform = `rotateY(${currentDegrees}deg)`;
 
-    coin.style.transform = `rotateY(${currentDegrees}deg)`;
-
-    setTimeout(() => {
-        red.innerHTML = `${amountRedAmount}`;
-        blue.innerHTML = `${amountBlueAmount}`;
-        isFlipping = false;
-        coin.classList.remove("flipping");
-    }, 5000);
-}
+  setTimeout(() => {
+    isFlipping = false;
+    coin.classList.remove('flipping');
+  }, 5000);
+};
